dedff0d3b963bd59f27ca6940f72faa115dfcb33
e94046165 <e94046165@mail.ncku.edu.tw>
2019-11-17 13:43:55 +0800

    finish HW3

--- Fintech/HW3/myOptimAction.py
+++ Fintech/HW3/myOptimAction.py
@@ -1,77 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
 import numpy as np
 
 def myOptimAction(priceMat, transFeeRate):
     # Explanation of my approach:
-	# 1. Technical indicator used: Watch next day price
-	# 2. if next day price > today price + transFee ==> buy
-    #       * buy the best stock
-	#    if next day price < today price + transFee ==> sell
-    #       * sell if you are holding stock
-    # 3. You should sell before buy to get cash each day
-    # default
-    cash = 1000
-    hold = 0
-    # user definition
-    nextDay = 1
+    # Using DP to find the optimal solution
+    # Def: 
+    #      cashMax[day] = max(for stock in range(stockCount):
+    #                           stockMaxHolding[day-1][stock]*(1 - transFeeRate)*priceMat[day][stock]),
+    #                       , Maxcash[day - 1])
+    #      stockMaxHolding[day][stock] = max(stockMaxHolding[day - 1][stock] 
+    #                                     ,cashMax[day]*(1 - transFeeRate)/priceMat[day][stock])
+    #以cashMax,stockMaxHolding 找出買賣點：
+    #　　由後往前看,第一次出現　cashMax[day] != cashMax[day - 1]　即為最後一個賣點
+    #  從賣點由後往前看出現　stockMaxHolding[day][hold] != stockMaxHolding[day-1][hold] 為買點
+    #　從買點由後往前看出現　cashMax[day] != cashMax[day - 1]　即為最後買點
+    #  
     dataLen, stockCount = priceMat.shape  # day size & stock count   
-    print(priceMat.shape)
-    stockHolding = np.zeros((dataLen,stockCount))  # Mat of stock holdings
+    #print(dataLen)
+    cashMax = np.zeros(dataLen-1)
+    cashMax[0] = 1000
+    stockMaxHolding = np.zeros((dataLen,stockCount))  # Mat of stock holdings
+
     actionMat = []  # An k-by-4 action matrix which holds k transaction records.
-    
-    for day in range(0, dataLen-nextDay ) :
-        dayPrices = priceMat[day]  # Today price of each stock
-        nextDayPrices = priceMat[ day + nextDay ]  # Next day price of each stock
-        
+    # user definition
+
+    for day in range(dataLen - 1):
+        action = []
         if day > 0:
-            stockHolding[day] = stockHolding[day-1]  # The stock holding from the previous action day
-        
-        buyStock = -1  # which stock should buy. No action when is -1
-        buyPrice = 0  # use how much cash to buy
-        sellStock = []  # which stock should sell. No action when is null
-        sellPrice = []  # get how much cash from sell
-        bestPriceDiff = 0  # difference in today price & next day price of "buy" stock
-        stockCurrentPrice = 0  # The current price of "buy" stock
-        value = cash
-        # Check next day price to "sell"
-        for stock in range(stockCount) :
-            todayPrice = dayPrices[stock]  # Today price
-            nextDayPrice = nextDayPrices[stock]  # Next day price
-            holding = stockHolding[day][stock]  # how much stock you are holding
-            value += holding*todayPrice
-            if holding > 0 :  # "sell" only when you have stock holding
-                if nextDayPrice < todayPrice*(1+transFeeRate) :  # next day price < today price, should "sell"
-                    sellStock.append(stock)
-                    # "Sell"
-                    sellPrice.append(holding * todayPrice)
-                    cash = holding * todayPrice*(1-transFeeRate) # Sell stock to have cash
-                    stockHolding[day][sellStock] = 0
-        
-        # Check next day price to "buy"
-        if cash > 0 :  # "buy" only when you have cash
-            for stock in range(stockCount) :
-                todayPrice = dayPrices[stock]  # Today price
-                nextDayPrice = nextDayPrices[stock]  # Next day price
-                
-                if nextDayPrice > todayPrice*(1+transFeeRate) :  # next day price > today price, should "buy"
-                    diff = (nextDayPrice - todayPrice*(1+transFeeRate))*(value*(1-transFeeRate) / todayPrice)
-                    if diff > bestPriceDiff :  # this stock is better
-                        bestPriceDiff = diff
-                        buyStock = stock
-                        stockCurrentPrice = todayPrice
-            # "Buy" the best stock
-            if buyStock >= 0 :
-                buyPrice = cash
-                stockHolding[day][buyStock] = cash*(1-transFeeRate) / stockCurrentPrice # Buy stock using cash
-                cash = 0
-                
-        # Save your action this day
-        if buyStock >= 0 or len(sellStock) > 0 :
-            action = []
-            if len(sellStock) > 0 :
-                for i in range( len(sellStock) ) :
-                    action = [day, sellStock[i], -1, sellPrice[i]]
-                    actionMat.append( action )
-            if buyStock >= 0 :
-                action = [day, -1, buyStock, buyPrice]
-                actionMat.append( action )
-    return actionMat
+            cash_best = cashMax[day-1]
+            hold = -1
+            for stock in range(stockCount):
+                if cash_best < stockMaxHolding[day-1][stock]*(1 - transFeeRate)*priceMat[day][stock]:
+                    cash_best = stockMaxHolding[day-1][stock]*(1 - transFeeRate)*priceMat[day][stock]
+            cashMax[day] = cash_best    
+            for stock in range(stockCount):   
+                if stockMaxHolding[day - 1][stock] < cashMax[day]*(1 - transFeeRate)/priceMat[day][stock]:
+                    stockMaxHolding[day][stock] = cashMax[day]*(1 - transFeeRate)/priceMat[day][stock]
+                else:
+                    stockMaxHolding[day][stock] = stockMaxHolding[day - 1][stock]
+    day = dataLen - 2
+    while day != 0:
+        hold = -1
+        #sell
+        while True:
+            if cashMax[day] != cashMax[day - 1]:
+                break
+            else:
+                day -= 1
+        if day > 0:
+            for stock in range(stockCount):
+                if stockMaxHolding[day-1][stock]*(1 - transFeeRate)*priceMat[day][stock] == cashMax[day]:
+                    action = [day, stock, -1, cashMax[day]/(1-transFeeRate)]
+                    actionMat.append(action)
+                    hold = stock
+                    break
+        #buy
+        while True:
+            if stockMaxHolding[day][hold] != stockMaxHolding[day-1][hold]:
+                break
+            else:
+                if day == 0:
+                    break
+                day -= 1
+        if day > 0:
+            action = [day, -1, hold, cashMax[day]]
+            actionMat.append(action)
+    return actionMat[::-1]
+
+
+
+
+
